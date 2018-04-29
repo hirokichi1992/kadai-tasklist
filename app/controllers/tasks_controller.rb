3e88a6bb4caf650eb66a787f8ef716ae49e6af4c
EC2 Default User <ec2-user@ip-172-31-26-134.us-east-2.compute.internal>
2018-04-29 13:03:10 +0000

    code brush up after mentering

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,24 @@
 class TasksController < ApplicationController
-  before_action :set_task, only:[:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only:[:new, :create, :show, :edit, :update, :destroy]
+  #before_action :set_task, only:[:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in, except: :index
   before_action :correct_user, only:[:show, :edit, :update, :destroy]
+
   def index
-    @tasks = Task.order(created_at: :desc).page(params[:page])
+    @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]) if logged_in?
   end
+
   def new
     @task = Task.new
   end
+
   def show
     #@task = Task.find(params[:id])
   end
+
   def edit
     #@task = Task.find(params[:id])
   end
+
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
@@ -24,6 +29,7 @@ class TasksController < ApplicationController
       render :new
     end
   end
+
   def update
     #@task = Task.find(params[:id])
     if @task.update(task_params)
@@ -34,6 +40,7 @@ class TasksController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     #@task = Task.find(params[:id])
     @task.destroy
